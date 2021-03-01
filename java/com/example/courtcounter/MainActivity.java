3852f5bcae78f23c38cb368e386c99880f9ab1d9
Gaurav Lamba <66568958+Gauravlamba1109@users.noreply.github.com>
2021-03-02 01:09:19 +0530

    Add files via upload

---
+++ java/com/example/courtcounter/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.courtcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    int scoreTeamA=0;
+    int scoreTeamB=0;
+
+    public void updateScoreBy3a(View view){
+        scoreTeamA = scoreTeamA+3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void updateScoreBy2a(View view){
+        scoreTeamA = scoreTeamA+2;
+        displayForTeamA(scoreTeamA);
+    }
+    public void updateScoreBy1a(View view){
+        scoreTeamA = scoreTeamA+1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void updateScoreBy3b(View view){
+        scoreTeamB = scoreTeamB+3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void updateScoreBy2b(View view){
+        scoreTeamB = scoreTeamB+2;
+        displayForTeamB(scoreTeamB);
+    }
+    public void updateScoreBy1b(View view){
+        scoreTeamB = scoreTeamB+1;
+        displayForTeamB(scoreTeamB);
+    }
+    public void reset(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamB(scoreTeamB);
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int scoreTeamA) {
+        TextView scoreViewA = (TextView) findViewById(R.id.team_a_score);
+        scoreViewA.setText(String.valueOf(scoreTeamA));
+    }
+
+    public void displayForTeamB(int scoreTeamB) {
+        TextView scoreViewB = (TextView) findViewById(R.id.team_b_score);
+        scoreViewB.setText(String.valueOf(scoreTeamB));
+    }
+
+
+}
